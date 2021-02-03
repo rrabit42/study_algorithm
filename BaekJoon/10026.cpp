53f7fea5fbb7cb435fb571f6c188c6ce3158e2bf
SeoYoung Kim <46364778+rrabit42@users.noreply.github.com>
2021-02-03 16:40:23 +0900

    Update 10026.cpp

--- BaekJoon/10026.cpp
+++ BaekJoon/10026.cpp
@@ -1,66 +1,131 @@
+#include <algorithm>
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
- 
+#include <queue>
+
 using namespace std;
-int count1 = 0, count2 = 0, n;
- 
-void DFS(int **arr, int num, int x, int y) {
-    arr[y][x] = 0;
-    if (y > 0 && arr[y - 1][x] == num)
-        DFS(arr, num, x, y - 1);
-    if (y < n - 1 && arr[y + 1][x] == num)
-        DFS(arr, num, x, y + 1);
-    if (x > 0 && arr[y][x - 1] == num)
-        DFS(arr, num, x - 1, y);
-    if (x < n - 1 && arr[y][x + 1] == num)
-        DFS(arr, num, x + 1, y);
+
+// 일단 문제 이해가 잘 안됨ㅋㅋ -> R와 G를 구분 못한다는 소리였다!
+
+/*
+#define MAX 100
+int n;
+int nor_arr[MAX][MAX];
+int rg_arr[MAX][MAX];
+int visited[MAX][MAX]; // 0으로 초기화
+queue<pair<int, int>> q;
+int nor_cnt = 0, rg_cnt = 0;
+int direct[4][2] = { {1,0},{-1,0},{0,1},{0,-1} };
+
+// 1. 언제 구역 더하기를 해야 하는가
+void BFS(int row, int column, int (*arr)[MAX]) {
+
+	q.push(make_pair(row, column));
+
+	while (!q.empty()) {
+		pair<int, int> now = q.front();
+		q.pop();
+
+		int color = arr[now.first][now.second];
+
+		// queue안에 들어는 갔지만 이미 탐색이 끝난 경우
+		if (visited[now.first][now.second] == 1) continue;
+
+		visited[row][column] = 1;
+
+		for (int i = 0; i < 4; i++) {
+			int next_row = now.first + direct[i][0];
+			int next_column = now.second + direct[i][1];
+
+			// 범위를 벗어난 좌표, 이미 탐색이 끝난 좌표
+			if (next_row<0 || next_row>n || next_column<0 || next_column >n || visited[next_row][next_column] == 1) {
+				continue;
+			}
+
+			// 같은 (색)구역 모두 찾고
+			if (color == arr[next_row][next_column]) {
+				visited[next_row][next_column] = 1;
+			}
+
+			// 다른 구역으로 넘어가기
+			else {
+				q.push(make_pair(next_row, next_column));
+			}
+		}
+		nor_cnt += 1;
+	}
 }
- 
+*/
+
+int n, nor_cnt = 0, rg_cnt = 0;
+// 이렇게 하니까 메모리 초과남
+// int nor_arr[MAX][MAX];
+// int rg_arr[MAX][MAX];
+
+// ** BFS로는 어려워서 라업보고 DFS로,,!ㅜㅜ
+// 나중에는 혼자서 풀고 싶다,,
+void DFS(int **arr, int num, int x, int y) {
+	arr[y][x] = 0;	// visited 표시?
+	if (y > 0 && arr[y - 1][x] == num)
+		DFS(arr, num, x, y - 1);
+	if (y < n - 1 && arr[y + 1][x] == num) // n-1 해야 y+1이 index 범위에서 벗어나지 않으니까
+		DFS(arr, num, x, y + 1);
+	if (x > 0 && arr[y][x - 1] == num)
+		DFS(arr, num, x - 1, y);
+	if (x < n - 1 && arr[y][x + 1] == num)
+		DFS(arr, num, x + 1, y);
+}
+
 int main() {
-    string str;
-    cin >> n;
-    int **arr1 = new int*[n], **arr2 = new int*[n];
- 
-    for (int i = 0; i < n; i++) {
-        arr1[i] = new int[n];
-        arr2[i] = new int[n];
-    }
- 
-    for (int i = 0; i < n; i++) {
-        cin >> str;
-        for (int j = 0; j < n; j++) {
-            switch (str[j])
-            {
-            case 'R':
-                arr1[i][j] = 1;
-                arr2[i][j] = 1;
-                break;
-            case 'G':
-                arr1[i][j] = 2;
-                arr2[i][j] = 1;
-                break;
-            case 'B':
-                arr1[i][j] = 3;
-                arr2[i][j] = 2;
-                break;
-            }
-        }
-    }
- 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (arr1[i][j] != 0) {
-                count1++;
-                DFS(arr1, arr1[i][j], j, i);
-            }
-            if (arr2[i][j] != 0) {
-                count2++;
-                DFS(arr2, arr2[i][j], j, i);
-            }
-        }
-    }
-    cout << count1 << " " << count2;
-    return 0;
+	cin >> n;
+
+	// ** whitespace로 구분하는데 애초에 문자열로 들어옴... 와 이걸 몰랐네
+	string str;
+	int **nor_arr = new int*[n], **rg_arr = new int*[n];
+
+	for (int i = 0; i < n; i++) {
+		nor_arr[i] = new int[n];
+		rg_arr[i] = new int[n];
+	}
+
+	// ** 색깔 단어 그대로 안받고 숫자로 바꿈
+	// ** 그럼 적록색맹은 그냥 R과 G를 같은 숫자로 표기하면 됨!!
+	for (int i = 0; i < n; i++) {
+		cin >> str;
+		for (int j = 0; j < n; j++) {
+			switch (str[j])
+			{
+			case 'R':
+				nor_arr[i][j] = 1;
+				rg_arr[i][j] = 1;
+				break;
+			case 'G':
+				nor_arr[i][j] = 2;
+				rg_arr[i][j] = 1;
+				break;
+			case 'B':
+				nor_arr[i][j] = 3;
+				rg_arr[i][j] = 3;
+				break;
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (nor_arr[i][j] != 0) {
+				nor_cnt++;
+				DFS(nor_arr, nor_arr[i][j], j, i);
+			}
+			if (rg_arr[i][j] != 0) {
+				rg_cnt++;
+				DFS(rg_arr, rg_arr[i][j], j, i);
+			}
+		}
+	}
+
+	cout << nor_cnt << ' ' << rg_cnt << '\n';
+	return 0;
 }
