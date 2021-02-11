9e73b0082e0b97e359f8b1e1de1b3bb5b87a8c2d
SeoYoung Kim <46364778+rrabit42@users.noreply.github.com>
2021-02-11 14:06:07 +0900

    [백준] 1003번-피보나치 수열(더 좋은 풀이)

--- BaekJoon/1003.cpp
+++ BaekJoon/1003.cpp
@@ -63,3 +63,41 @@ int main() {
 		cout << arr[n][0] << ' ' << arr[n][1] << '\n';
 	}
 }
+
+/*
+규칙성 찾아서 쉽게 풀기
+0의 출력 횟수 1, 0, 1, 1, 2, 3...
+1의 출력 횟수 0, 1, 1, 2, 3, 5...
+
+피보나치처럼 앞 횟수 더해가는게 맞음!(내풀이)
+그렇기에 각자 서로 피보나치 수열을 또다시 이루는것.
+
+즉, 0의 출력횟수는 N==0일때 1을 제외하고 0부터 시작하는 피보나치 수열,
+1의 출력횟수는 N==0일 때부터 시작하는 피보나치 수열이다!
+*/
+
+#include <iostream>
+using namespace std;
+long arr[50] = {0, 1, }; // 초기값을 걍 여기서 바로 줄 수 있네
+
+long fibo(int n){
+	if(n == 0 || n == 1)
+		return arr[n];
+	else if(arr[n] == 0)
+		arr[n] = fibo(n-1) + fibo(n-2);
+	return arr[n];
+}
+
+int main(){
+	int t;
+	cin >> t;
+	
+	int tmp;
+	for(int i=0; i<t; i++){
+		cin >> tmp;
+		if(tmp == 0)
+			cout << "1 0" << '\n';
+		else
+			cout << fibo(tmp-1) << ' ' << fibo(tmp) << '\n';
+	}
+}
