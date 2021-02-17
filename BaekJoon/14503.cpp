8c1ec137abeb06d3b3743e9aaba4a33b893d7b85
SeoYoung Kim <46364778+rrabit42@users.noreply.github.com>
2021-02-17 18:46:37 +0900

    Update 14503.cpp

--- BaekJoon/14503.cpp
+++ BaekJoon/14503.cpp
@@ -1,121 +1,80 @@
 #include <iostream>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#define MAX 50
+
 using namespace std;
- 
-// U, R, D, L        (0, 1, 2, 3)
-int N, M , robot_x, robot_y, robot_direction, moved_area = 0;
-int next_x, next_y;
-int** map;
-int round_val[4][2] = { { -1, 0 },{ 0, 1 },{ 1 , 0 },{ 0, -1 } };
- 
-// 뒤쪽으로 이동이 가능한지 확인하는 함수 
-int isBackPossible() {
-    // 뒤쪽 값 
-    next_x = robot_x + round_val[(robot_direction + 2) % 4][0];
-    next_y = robot_y + round_val[(robot_direction + 2) % 4][1];
- 
-    // 뒤쪽이 없다. 
-    if (next_x < 0 || next_x >= N || next_y < 0 || next_y >= M) return 0; 
-    // 뒤쪽이 벽이다. 
-    else if (map[next_x][next_y] == 1) return 0; 
-    else {
-        return 1;
-    }
-}
- 
-// 주변이 비었는지 확인하는 함수 
-int isRoundEmpty() {
-    int roundFull = 0 ;
-    for (int i = 0; i < 4; i++) {
-        // 왼쪽 값 
-        next_x = robot_x + round_val[(robot_direction + i + 3) % 4][0];
-        next_y = robot_y + round_val[(robot_direction + i + 3) % 4][1];
- 
-        // 왼쪽이 없다. 
-        if (next_x < 0 || next_x >= N || next_y < 0 || next_y >= M) roundFull++;
-        // 왼쪽이 이미 청소되어있다.
-        else if (map[next_x][next_y] == 7) roundFull++;
-        // 왼쪽이 벽이다. 
-        else if (map[next_x][next_y] == 1) roundFull++;
-        // 왼쪽이 청소되어있지 않다.
-        else if (map[next_x][next_y] == 0) return i;
-    }
- 
-    if (roundFull == 4) return 0;
-}
- 
-int howStatusLeft() {
-    // 왼쪽 값 
-    next_x = robot_x + round_val[(robot_direction + 3) % 4][0];
-    next_y = robot_y + round_val[(robot_direction + 3) % 4][1];
- 
-    // 왼쪽이 없다. 
-    if (next_x < 0 || next_x >= N || next_y < 0 || next_y >= M) return 2;
-    // 왼쪽이 이미 청소되어있다.
-    else if (map[next_x][next_y] == 7) return -1;
-    // 왼쪽이 벽이다. 
-    else if (map[next_x][next_y] == 1) return 1;
-    // 왼쪽이 청소되어있지 않다.
-    else if (map[next_x][next_y] == 0) return 0;
-}
- 
-int move() {
-    // 1. 현재 위치를 청소한다. 
-    if (map[robot_x][robot_y] == 0) { 
-        map[robot_x][robot_y] = 7;
-        moved_area++;
-    }
- 
-    // 2. 현재 위치에서 현재 방향을 기준으로 왼쪽방향부터 차례대로 탐색을 진행한다. 
-    // - 왼쪽 방향에 아직 청소하지 않은 공간이 존재한다면, 그 방향으로 회전한 다음 한 칸을 전진하고 1번부터 진행한다.
-    int left_status = howStatusLeft();
-    int is_round_empty = isRoundEmpty();
-    if (left_status == 0) {
-        // 왼쪽으로 회전 
-        robot_direction = (robot_direction + 3) % 4;
-        robot_x = next_x;
-        robot_y = next_y;
-    }
-    // 네 방향 모두가 청소 되있거나 벽인 경우에는 
-    else if (!is_round_empty) {
-        //    - 네 방향 모두 청소가 이미 되어있거나 벽인 경우에는, 바라보는 방향을 유지한 채로 한 칸 후진을 하고 2번으로 돌아간다.
-        if (isBackPossible()) {
-            robot_x = robot_x + round_val[(robot_direction + 2) % 4][0];
-            robot_y = robot_y + round_val[(robot_direction + 2) % 4][1];
-        }
-        //    - 네 방향 모두 청소가 이미 되어있거나 벽이면서, 뒤쪽 방향이 벽이라 후진도 할 수 없는 경우에는 작동을 멈춘다.
-        else {
-            return 0;
-        }
-    } 
-    // - 왼쪽 방향에 청소할 공간이 없다면, 그 방향으로 회전하고 2번으로 돌아간다.
-    else if(left_status == 1 || left_status == -1) {
-        // 왼쪽으로 회전 
-        robot_direction = (robot_direction + 3) % 4;
-    }
- 
-    return 1;
-}
- 
+
+int n, m;
+int r, c, d, nr, nc, nd;
+int area[MAX][MAX]; // 0: 빈칸(청소X), 1: 벽, 2: 청소함
+int result, cnt;
+
+// ** switch로 했었는데 이걸 그냥 정해서 주는 것
+// 북(0) 동(1) 남(2) 서(3) -> 인덱스로 방향 알 수 있게
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+
 int main() {
-    cin >> N >> M;
-    cin >> robot_x >> robot_y >> robot_direction;
- 
-    // 맵 구성
-    map = (int**)malloc(sizeof(int*)*N);
-    for (int i = 0; i < N; i++) {
-        map[i] = (int*)malloc(sizeof(int)*M);
-    }
- 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            cin >> map[i][j];
-        }
-    }
- 
-    while (move()) { 
-    }
-     
-    cout << moved_area;
- 
-    return 0; 
+	// 세로크기 n, 가로크기 m
+	cin >> n >> m;
+
+	// 로봇청소기가 있는 칸 좌표(r, c), 바라보는 방향 d
+	// 0: 북, 1: 동, 2: 남, 3: 서
+	cin >> r >> c >> d;
+
+	// 빈칸 0, 벽 1
+	// 지도의 첫행, 마지막행, 첫열, 마지막열은 모두 벽
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> area[i][j];
+		}
+	}
+
+	while (true) {
+		// 1. 현재 위치 청소
+		if (area[r][c] == 0) {
+			area[r][c] = 2; // 청소
+			result++;
+		}
+
+		// 2. 현재 방향 기준 왼쪽 방향부터 차례대로 탐색
+			// 서-북-동-남 이 area에서도 동일(세로는 북->남, 가로는 동->서 이므로)
+			// 즉, 왼쪽은 서로 3-2-1-0-3...
+		bool flag = false; // 왼쪽 방향으로 전진 가능한지 여부
+		for (int i = 0; i < 4; i++) {
+			d = (d + 3) % 4;
+
+			nr = r + dx[d];
+			nc = c + dy[d];
+
+		// - 왼쪽 청소 X: 그 방향으로 회전, 1칸 전진, 1번으로!
+			if (area[nr][nc] == 0) {
+				r = nr; c = nc;
+				flag = true;
+				break;
+			}
+		}
+
+		// 전진했음. 1번으로 돌아가기
+		if (flag) continue;
+
+		// 4면이 모두 청소 불가일 때(벽 or 청소됨)
+		// 후진 가능: 바라보는 방향 유지, 한칸 후진, 2번으로!(while 반복)
+		nd = (d + 2) % 4; // 후진 방향
+		r = r + dx[nd];
+		c = c + dy[nd];
+
+		// 후진 불가: 작동 불가
+		if (area[r][c] == 1) break;
+	}
+
+	cout << result << '\n';
 }
+
+/*
+ 처음에 조건 4개를 따로 따로 구현하려 했는데
+ 가지치기 해서 조건을 파악해서 if else로 적절히 나누면 중복해서 하지 않아도 하나로 깔끔하게 동작 된다.
+ 여러모로 여러번 복습하면 좋을 문제인듯
+*/
