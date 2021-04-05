a414b379bc3e1cbe18190ca216dca39a66db2b83
SeoYoung Kim <46364778+rrabit42@users.noreply.github.com>
2021-04-05 14:44:03 +0900

    [컴퓨터알고리즘] 4주차: 선택 정렬, 삽입 정렬, 버블 정렬 + 쉘 정렬

--- 컴퓨터알고리즘/4주차/정렬.cpp
+++ 컴퓨터알고리즘/4주차/정렬.cpp
@@ -47,6 +47,31 @@ void bubble_sort(int list[], int n)
 	}
 }
 
+void inc_insertion_sort(int list[], int first, int last, int gap)
+{
+	int i, j, key;
+	for (i = first+gap; i <=last; i=i+gap)
+	{
+		key = list[i];
+		for (j = i - gap; j >= first && list[j] > key; j = j-gap)
+			list[j + gap] = list[j];
+		list[j + gap] = key;
+	}
+}
+
+void shell_sort(int list[], int n)
+{
+	int i, gap;
+	for (gap = n / 2; gap > 0; gap = gap / 2)
+	{
+		// gap이 홀수일 때가 효율이 더 높으니까
+		if (gap % 2 == 0)
+			gap++;
+		for (i = 0; i < gap; i++)
+			inc_insertion_sort(list, i, n - 1, gap);
+	}
+}
+
 int main()
 {
 	int list[MAX_SIZE];
@@ -69,7 +94,8 @@ int main()
 
 	// selection_sort(list, MAX_SIZE);
 	// insertion_sort(list, MAX_SIZE);
-	bubble_sort(list, MAX_SIZE);
+	// bubble_sort(list, MAX_SIZE);
+	shell_sort(list, MAX_SIZE);
 
 	for (int i = 0; i < MAX_SIZE; i++)
 	{
