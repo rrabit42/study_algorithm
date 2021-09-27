2750a08424cb8f14038525af848d05f5024b6132
SeoYoung Kim <46364778+rrabit42@users.noreply.github.com>
2021-09-27 18:13:36 +0900

    704. Binary Search - 정석 방법

--- LeetCode/Algorithm I/[Binary Search] 704. Binary Search.py
+++ LeetCode/Algorithm I/[Binary Search] 704. Binary Search.py
@@ -6,4 +6,23 @@ class Solution:
             return -1
 
 # 문제 의도는 이게 아니었을 것이다.
-# 진짜 Binary Search로 풀면 아래와 같다. (내일 풀어야징)
+# 진짜 Binary Search로 풀면 아래와 같다.
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # nums는 오름차순으로 정렬되어 있음
+        
+        left = 0
+        right = len(nums) - 1
+        
+        while(left <= right):
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return -1
+            
