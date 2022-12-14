8280e816cff8e0c1ba10e6dcee9efcc9c1239b86
SeoYoung Kim <46364778+rrabit42@users.noreply.github.com>
2022-12-15 02:22:58 +0900

    Update 같은숫자는싫어.py

--- Programmers/같은숫자는싫어.py
+++ Programmers/같은숫자는싫어.py
@@ -6,3 +6,13 @@ def solution(arr):
             answer.append(arr[i])
     
     return answer
+
+
+# 다른 사람 풀이
+# a[-1:]를 빈 배열에 써도 문제가 없다!
+def no_continuous(s):
+    a = []
+    for i in s:
+        if a[-1:] == [i]: continue
+        a.append(i)
+    return a
