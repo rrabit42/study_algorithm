a82f203f9557fafba28af88fc6a05ef73b781d8a
SeoYoung Kim <46364778+rrabit42@users.noreply.github.com>
2023-03-20 03:24:32 +0900

    Update K번째수.py

--- Programmers/K번째수.py
+++ Programmers/K번째수.py
@@ -8,3 +8,7 @@ def solution(array, commands):
         answer.append(subArr[k-1])
     
     return answer
+
+# 더 pythonic 한 코드
+def solution(array, commands):
+    return [sorted(array[i-1:j])[k-1] for i,j,k in commands]
