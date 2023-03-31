7687dfddc224b108b5e8c5840263d255b73b589f
SeoYoung Kim <46364778+rrabit42@users.noreply.github.com>
2023-04-01 02:54:21 +0900

    Update 10828.py

--- BaekJoon/10828.py
+++ BaekJoon/10828.py
@@ -27,3 +27,24 @@ for _ in range(N):
         print(-1)
       else:  
         print(stack[-1])
+
+        
+# 한줄로 쓸 수 있다는거 잊음
+# if stack, +(not stack) = +(True) = 0 + True = 0 + 1 = 1
+
+stack = []
+N = int(input())
+
+for n in range(N):
+    command = input().rstrip()
+    if command == "pop":
+        print(stack.pop() if stack else -1)
+    elif command == "size":
+        print(len(stack))
+    elif command == "empty":
+        print(+(not stack))
+    elif command == "top":
+        print(stack[-1] if stack else -1)
+    else:
+        _, x = command.split()
+        stack.append(x)
