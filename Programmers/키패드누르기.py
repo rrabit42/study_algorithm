4cbfef907b40596ed0a699b8141a1605807ce2dd
SeoYoung Kim <46364778+rrabit42@users.noreply.github.com>
2023-07-06 21:20:03 +0900

    Update 키패드누르기.py

--- Programmers/키패드누르기.py
+++ Programmers/키패드누르기.py
@@ -57,3 +57,51 @@ def solution(numbers, hand):
             right.append(num)
         
     return answer
+
+############################
+# 굳이 deque 쓸 필요 없음
+keypad = {
+    1: [0,0], 2: [0,1], 3: [0,2],
+    4: [1,0], 5: [1,1], 6: [1,2],
+    7: [2,0], 8: [2,1], 9: [2,2],
+    "*": [3,0], 0: [3,1], "#": [3,2]
+}
+
+def solution(numbers, hand):
+    answer = ''
+    left = '*'
+    right = '#'
+    
+    act = ''
+    for num in numbers:
+        if num in [1, 4, 7]:
+            act = 'left'
+        elif num in [3, 6, 9]:
+            act = 'right'
+        else: # 2, 5, 8, 0
+            # 좌표 거리 계산
+            x, y = keypad[num]
+            lx, ly = keypad[left]
+            rx, ry = keypad[right]
+            left_dis = abs(lx - x) + abs(ly - y)
+            right_dis = abs(rx - x) + abs(ry - y)
+            
+            # L과 가까운 경우
+            if left_dis < right_dis:
+                act = 'left'
+            # R과 가까운 경우
+            elif right_dis < left_dis:
+                act = 'right'
+            # 동일한 거리인 경우
+            else:
+                act = hand
+
+        # 선택한 손에 맞는 행동하기
+        if act == 'left':
+            answer += 'L'
+            left = num
+        else:
+            answer += 'R'
+            right = num
+        
+    return answer
