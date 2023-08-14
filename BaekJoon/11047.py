fbfe76c96539887cf685e530851ff6fe1829a0c1
SeoYoung Kim <46364778+rrabit42@users.noreply.github.com>
2023-08-14 15:09:44 +0900

    Update 11047.py

--- BaekJoon/11047.py
+++ BaekJoon/11047.py
@@ -1,3 +1,5 @@
+######################################
+# 2023.04.25
 import sys
 
 input = sys.stdin.readline
@@ -16,3 +18,20 @@ for coin in coins: # 당연하게 큰 수부터 나누면 최솟값이 나옴
   left = left % coin
   
 print(count)
+==========================================
+# 2023.08.14
+# 오 발전했는데ㅋㅋㅋㅋ
+
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+a = [int(input()) for _ in range(n)] # 오름차순
+
+num = 0
+for i in range(n-1, -1, -1):
+  if k >= a[i]:
+    p, k = divmod(k, a[i])
+    num += p
+
+print(num)
