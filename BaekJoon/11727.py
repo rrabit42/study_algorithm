b23c3e3dd455b1a08d3f58356ffe5f19cde52d9d
SeoYoung Kim <46364778+rrabit42@users.noreply.github.com>
2023-08-20 01:59:23 +0900

    [백준] 2xn 타일링 2

--- BaekJoon/11727.py
+++ BaekJoon/11727.py
@@ -9,11 +9,20 @@ n = int(input())
 
 # 2xn을 1x2, 2x1, 2x2 로 채우는 방법
 dp = [0 for _ in range(n+1)]
-dp[1], dp[2] = 1, 3
+dp[0], dp[1] = 1, 1
 
-for i in range(3, n+1):
+for i in range(2, n+1):
   # dp[i-1]에서 1x2가 하나 늘어난 개수 +
   # dp[i-2]에서 2x2의 경우의 수 2개(2x2 or 2x1 * 2) -> 위에꺼에 없는 경우로 알아서 채워짐
   dp[i] = dp[i-1] + dp[i-2] * 2
   
-print(dp[n])
+print(dp[n]%10007)
+
+#####################
+# append로 하면 될듯
+
+n = int(input())
+dp=[0,1,3]
+for i in range(3,n+1):
+    dp.append(dp[i-1] + dp[i-2]*2)
+print(dp[n]%10007)
