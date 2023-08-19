ceae78a72964bc4aa23d58a36fe950522801dc2a
SeoYoung Kim <46364778+rrabit42@users.noreply.github.com>
2023-08-19 16:22:00 +0900

    Update 1931.py

--- BaekJoon/1931.py
+++ BaekJoon/1931.py
@@ -34,7 +34,36 @@ for e, s in dict.items():
 
 print(num)
 
+########################################
+# 깍두기가 여러개일 경우, min 작업 때문에 하나의 경우의 수로 뭉개짐
+# 틀린 풀이!
+import sys
+from collections import OrderedDict
+input = sys.stdin.readline
 
+n = int(input())
+m = {}
+
+for _ in range(n):
+  s, e = map(int, input().split())
+  if s in m:
+    m[s] = min(m[s], e) # 같은 시간에 시작하면 빨리 끝나는게 낫지
+  else:
+    m[s] = e
+
+# 빨리 끝나는 순으로 정렬
+m = OrderedDict(sorted(m.items(), key=lambda x: x[1], reverse=False))
+num, prev_end = 0, 0
+
+# 그 시간에 회의 개수 최대
+for s, e in m.items():
+  if s >= prev_end:
+    num +=1
+    prev_end = e
+
+print(num)
+
+#########################################
 # 베스트 풀이
 # 회의실 끝 시간부터 살펴보기
 import sys
@@ -52,3 +81,26 @@ def main():
     print(result)
 
 main()
+#####################################
+# 정렬만 잘해도 풀 수 있는 문제!
+# 순서대로 빈틈없이 회의실을 사용하면 된다. 앞에서든, 뒤에서든!
+
+N = int(input())
+time = []
+
+for _ in range(N):
+  start, end = map(int, input().split())
+  time.append([start, end])
+
+time = sorted(time, key=lambda a: a[0]) # 시작 시간을 기준으로 오름차순
+time = sorted(time, key=lambda a: a[1]) # 끝나는 시간을 기준으로 다시 오름차순
+
+last = 0 # 회의의 마지막 시간을 저장할 변수
+conut = 0 # 회의 개수를 저장할 변수
+
+for i, j in time:
+  if i >= last: # 시작시간이 회의의 마지막 시간보다 크거나 같을경우
+    conut += 1
+    last = j
+
+print(conut)
