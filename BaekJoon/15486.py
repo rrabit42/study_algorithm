ec601bb9b9e9bb37aa1cf5df4ec53d62b8d9bf95
SeoYoung Kim <46364778+rrabit42@users.noreply.github.com>
2023-08-20 12:24:09 +0900

    [백준] 퇴사 2

---
+++ BaekJoon/15486.py
@@ -0,0 +1,38 @@
+# 퇴사1과 다른건, 그때는 경우의 수가 작아서 dfs로도 풀 수 있었다는 것
+# 근데 애초가 dp로 풀었기에 풀이가 똑같음...ㅎㅎ
+
+# top-down
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+s = [(*map(int, input().split()),) for _ in range(n)]
+dp = [0 for _ in range(n+1)] # n일 차 때 최대 이익 -> 0일 때 최대 이익이 되도록 만들어 나감
+
+# 거꾸로 생각하기
+for i in range(n-1, -1, -1):
+  if i + s[i][0] <= n:
+    # 그대로 이어 받기 vs 오늘 상담 진행하기
+    dp[i] = max(dp[i+s[i][0]] + s[i][1], dp[i+1])
+  else:
+    dp[i] = dp[i+1]
+
+print(dp[0])
+
+########################
+# bottom-up은 시간 초과...
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+s = [(*map(int, input().split()),) for _ in range(n)]
+dp = [0 for _ in range(n+1)] # n일 차 때 최대 이익
+
+for i, (t, p) in enumerate(s):
+  for j in range(i+t, n+1):
+    if dp[j] <= dp[i] + p:
+      dp[j] = dp[i] + p
+
+print(dp)
+  
+
